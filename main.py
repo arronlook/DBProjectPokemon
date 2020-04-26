d52c936395667b1b77add9d7b8a15b0df63be884
Wilson Wong <wilsonwong1688@gmail.com>
2020-04-25 23:47:50 -0400

    minor changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from prompt_toolkit import prompt
 from prompt_toolkit.history import FileHistory
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.completion import WordCompleter
+import psycopg2
 import features
 
 main_menu_completer = WordCompleter(features.__functions__.keys(), ignore_case=True)
--- other_stuff/loadingCSV.py
+++ other_stuff/loadingCSV.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     db_cursor.execute("DROP TABLE IF EXISTS tbl_pokemon")
     db_cursor.execute("CREATE TABLE tbl_pokemon(abilities VARCHAR(256), against_bug real,against_dark real,against_dragon real,against_electric real,against_fairy real,against_fight real,against_fire real,against_flying real,against_ghost real,against_grass real,against_ground real,against_ice real,against_normal real,against_poison real,against_psychic real,against_rock real,against_steel real,against_water real,attack INT,base_egg_steps INT,base_happiness INT,base_total INT,capture_rate VARCHAR(255),classfication VARCHAR(255),defense INT,experience_growth INT,height_m VARCHAR(256),hp INT,japanese_name VARCHAR(255),name VARCHAR(255) PRIMARY KEY,percentage_male VARCHAR(256),pokedex_number INT,sp_attack INT,sp_defense INT,speed INT,type1 VARCHAR(255),type2 VARCHAR(255),weight_kg VARCHAR(256),generation INT,is_legendary INT)" )
-    f_contents = open('./pokemon_parsed.csv', 'r')
+    f_contents = open('./pokemon_parsed.csv', mode='r', encoding='utf-8')
     db_cursor.copy_from(f_contents, "tbl_pokemon",columns=('abilities','against_bug','against_dark','against_dragon','against_electric','against_fairy','against_fight','against_fire','against_flying','against_ghost','against_grass','against_ground','against_ice','against_normal','against_poison','against_psychic','against_rock','against_steel','against_water','attack','base_egg_steps','base_happiness','base_total','capture_rate','classfication','defense','experience_growth','height_m','hp','japanese_name','name','percentage_male','pokedex_number','sp_attack','sp_defense','speed','type1','type2','weight_kg','generation','is_legendary'), sep=",")
     db_conn.commit()
 
