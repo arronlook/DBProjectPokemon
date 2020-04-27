c5bee9e5d9254111ec88e3841d573052b5c87593
Victor Nazzaro <nazzav923@gmail.com>
2020-04-26 21:13:33 -0400

    Database singleton done

---
+++ database_conn.py
@@ -0,0 +1,25 @@
+import psycopg2
+
+class DB_conn:
+    class __DB_conn:
+        def __init__(self):
+            t_host = "localhost" # PostgreSQL database host address,either "localhost", a domain name, or an IP address.
+            t_port = "5432" # default postgres port
+            t_dbname = "pokemon" #database name
+            t_user = "ash" #database user name
+            t_pw = "ketchum" #password
+            self.db_conn = psycopg2.connect(host=t_host, port=t_port, dbname=t_dbname, user=t_user, password=t_pw)
+        
+        def __getConn(self):
+            return self.db_conn
+
+    instance = None
+    def __init__(self):
+        if not DB_conn.instance:
+            DB_conn.instance = DB_conn.__DB_conn()
+
+    @classmethod
+    def getConn(cls):
+        if not DB_conn.instance:
+            cls.instance = DB_conn.__DB_conn()
+        return cls.instance.__getConn()
--- features/victor.py
+++ features/victor.py
@@ -1,13 +1,20 @@
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter
 from namespaces import all_pokemon
+from database_conn import DB_conn
+
 def victor_feature1():
     pokemon_completer = WordCompleter(all_pokemon, ignore_case=True)
     option = prompt('Print a pokemon\'s name>', completer=pokemon_completer)
     print(option)
 
 def victor_feature2():
-    print("Victor feature 2")
+    connection = DB_conn.getConn()
+    cursor = connection.cursor()
+    query = "SELECT * FROM tbl_pokemon"
+    with cursor as cursor:
+        cursor.execute(query)
+        print(cursor.fetchall())
 
 __functions__ = {
     "victor_feature1": victor_feature1,
