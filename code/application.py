862b59ce0bff550986fe332b4b2043a6a9eedda7
arronlook <33300146+arronlook@users.noreply.github.com>
2020-05-06 14:50:35 -0400

    ThemeSong is optional

--- code/application.py
+++ code/application.py
@@ -4,14 +4,6 @@ from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.completion import WordCompleter
 import features
 
-# For the surprise
-import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-import pygame
-import threading
-
-from load_data import findFile
-
 main_menu_completer = WordCompleter(features.__functions__.keys(), ignore_case=True)
 
 def call_feature(feature: str):
@@ -22,14 +14,27 @@ def call_feature(feature: str):
 
 def main():
     try:
+        # For the surprise
+        import os
+        os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+        import pygame
+        import threading
+
+        from load_data import findFile
         pygame.mixer.init()
         pygame.mixer.music.set_volume(0.05)
         pygame.mixer.music.load(findFile("../themeSong.mp3")[1])
         pygame.mixer.music.play(-1)
         call_feature("logo")
         print("Turn up the volume if you can't hear it!", flush=True)
-    except e:
+    except FileNotFoundError as e:
         call_feature("logo")
+        print("Please check the readme and install the optional themesong. Also make sure that All_moves.csv exists in the /code/datasets directory.", flush=True)
+        print(e, flush=True)
+        print("Failed to play surprise theme song.", flush=True)
+    except Exception as e:
+        call_feature("logo")
+        print(e, flush=True)
         print("Failed to play surprise theme song.", flush=True)
 
     while 1:
